b78d3efa009f580b2dd36f9f1dbb9a4d94e310a1
Paulo Eduardo Pitta <paulopitta97@gmail.com>
2024-07-09 01:27:53 -0300

    fix: ajusta versão nativewind bug class name botão

--- mobile/src/components/button.tsx
+++ mobile/src/components/button.tsx
@@ -15,14 +15,13 @@ const ThemeContext = createContext<{variant?: Variants}>({})
 function Button({variant = "primary", children, isLoading, ...rest}: ButtonProps) {
     return (
         <TouchableOpacity 
-            /*className={clsx(
+            className={clsx(
                 "w-full h-11 flex-row items-center justify-center rounded-lg gap-2",
                 {
                     "bg-lime-300": variant === "primary",
                     "bg-zinc-800": variant === "secondary",
                 }
-            )}*/
-            className="bg-lime-300"
+            )}
             disabled={isLoading}
             activeOpacity={0.7}
             {...rest}>
