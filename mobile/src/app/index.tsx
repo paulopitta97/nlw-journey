89be99186e767364d2dee37c5a64ea4a930eac0a
Paulo Eduardo Pitta <paulopitta97@gmail.com>
2024-07-10 00:43:32 -0300

    feat: adiciona modal calendario datas

--- mobile/src/app/index.tsx
+++ mobile/src/app/index.tsx
@@ -1,23 +1,60 @@
-import { Input } from "@/components/input"
-import { View, Text, Image } from "react-native"
-import { MapPin, Calendar as IconCalendar, Settings2, UserRoundPlus, ArrowRight } from "lucide-react-native"
-import { colors } from "@/styles/colors"
-import { Button } from "@/components/button"
 import { useState } from "react"
+import { View, Text, Image, Keyboard, Alert } from "react-native"
+import { DateData } from "react-native-calendars"
+import { MapPin, Calendar as IconCalendar, Settings2, UserRoundPlus, ArrowRight } from "lucide-react-native"
+import dayjs from "dayjs"
+
+import { colors } from "@/styles/colors"
+import { calendarUtils, DatesSelected } from "@/utils/calendarUtils"
+
+import { Input } from "@/components/input"
+import { Button } from "@/components/button"
+import { Modal } from "@/components/modal"
+import { Calendar } from "@/components/calendar"
 
 enum StepForm {
     TRIP_DETAILS = 1,
     ADD_EMAIL = 2,
 }
 
+enum MODAL {
+    NONE = 0,
+    CALENDAR = 1,
+    GUESTS = 2
+}
+
 export default function Index() {
 
+    // Data/Form
     const [stepForm, setStepForm] = useState(StepForm.TRIP_DETAILS)
+    const [selectedDates, setSelectedDates] = useState({} as DatesSelected)
+    const [destination, setDestination] = useState("")
+
+    // Modal
+    const [showModal, setShowModal] = useState(MODAL.NONE);
 
     function handleNextStepForm() {
+        if(destination.trim().length === 0 || !selectedDates.startsAt || !selectedDates.endsAt ) {
+            return Alert.alert("Detalhes da viagem", "Preencha todas as informações da viagem para prosseguir.")
+        }
+
+        if(destination.length < 4) {
+            return Alert.alert("Detalhes da viagem", "O destino deve ter pelo menos 4 caracteres.")
+        }
+
         if(stepForm === StepForm.TRIP_DETAILS) {
             return setStepForm(StepForm.ADD_EMAIL)
         }
+    }
+
+    function handleSelectDate(selectedDay: DateData) {
+        const dates = calendarUtils.orderStartsAtAndEndsAt({
+            startsAt: selectedDates.startsAt,
+            endsAt: selectedDates.endsAt,
+            selectedDay: selectedDay
+        })
+
+        setSelectedDates(dates)
     }
 
     return (
@@ -41,12 +78,24 @@ export default function Index() {
             <View className="w-full bg-zinc-900 p-4 rounded-xl my-8 border border-zinc-800">
                 <Input>
                     <MapPin color={colors.zinc[400]} size={20} />
-                    <Input.Field placeholder="Para onde?" editable={stepForm === StepForm.TRIP_DETAILS} />
+                    <Input.Field 
+                        placeholder="Para onde?" 
+                        editable={stepForm === StepForm.TRIP_DETAILS} 
+                        onChangeText={setDestination}
+                        value={destination}
+                    />
                 </Input>
 
                 <Input>
                     <IconCalendar color={colors.zinc[400]} size={20} />
-                    <Input.Field placeholder="Quando?" editable={stepForm === StepForm.TRIP_DETAILS} />
+                    <Input.Field 
+                        placeholder="Quando?" 
+                        editable={stepForm === StepForm.TRIP_DETAILS} 
+                        onFocus={() => Keyboard.dismiss()} 
+                        showSoftInputOnFocus={false}
+                        onPressIn={() => stepForm === StepForm.TRIP_DETAILS && setShowModal(MODAL.CALENDAR)}
+                        value={selectedDates.formatDatesInText}
+                    />
                 </Input>
 
                 { stepForm === StepForm.ADD_EMAIL && (
@@ -78,6 +127,24 @@ export default function Index() {
             <Text className="text-zinc-500 font-regular text-center text-base">
                 Ao planejar sua viagem pela plann.er você automaticamente concorda com nossos{" "}<Text className="text-zinc-300 underline">termos de uso e políticas de privacidade.</Text>
             </Text>
+
+            <Modal 
+                title="Selecionar datas" 
+                subtitle="Selecione a data de ida e volta da viagem" 
+                visible={showModal === MODAL.CALENDAR}
+                onClose={() => setShowModal(MODAL.NONE)}
+            >
+                <View className="gap-4 mt-4">
+                    <Calendar 
+                        onDayPress={handleSelectDate}
+                        markedDates={selectedDates.dates}
+                        minDate={dayjs().toISOString()}
+                    />
+                    <Button onPress={() => setShowModal(MODAL.NONE)}>
+                        <Button.Title>Confirmar</Button.Title>
+                    </Button>
+                </View>
+            </Modal>
         </View>
     )
 }
