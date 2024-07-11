c9ef5b9d982c843f7054cf47cebe9ae314700434
Paulo Eduardo Pitta <paulopitta97@gmail.com>
2024-07-11 20:03:17 -0300

    feat: adiciona tela cadastro e listagem de atividade

--- mobile/src/components/button.tsx
+++ mobile/src/components/button.tsx
@@ -16,7 +16,7 @@ function Button({variant = "primary", children, isLoading, className, ...rest}: 
     return (
         <TouchableOpacity 
             className={clsx(
-                "h-11 flex-row items-center justify-center rounded-lg gap-2",
+                "h-11 flex-row items-center justify-center rounded-lg gap-2 px-2",
                 {
                     "bg-lime-300": variant === "primary",
                     "bg-zinc-800": variant === "secondary",
